c6d0e89d6a7accfe4e33b1e83d003be98d453630
HFYEH <sharefun010407@gmail.com>
2019-01-08 14:05:06 +0800

    Coding_dojo_00: Create project

---
+++ coding_dojo_00/coding_dojo_00/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  coding_dojo_00
+//
+//  Created by sharefun on 2019/1/8.
+//  Copyright © 2019 Theia Tech. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
